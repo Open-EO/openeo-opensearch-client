b5c448063b14e024df8782633cd9ebbea56a402a
Jan Van den bosch <jan@bossie.org>
2023-05-24 14:53:53 +0200

    clean up vars and redundant followRedirects
    https://github.com/Open-EO/openeo-geopyspark-driver/issues/402

--- src/main/scala/org/openeo/opensearch/backends/STACClient.scala
+++ src/main/scala/org/openeo/opensearch/backends/STACClient.scala
@@ -4,7 +4,6 @@ import org.openeo.opensearch.OpenSearchClient
 import org.openeo.opensearch.OpenSearchResponses.{Feature, FeatureCollection, STACCollections, STACFeatureCollection}
 import geotrellis.proj4.LatLng
 import geotrellis.vector.{Extent, ProjectedExtent}
-import scalaj.http.HttpOptions
 
 import java.net.URL
 import java.time.ZonedDateTime
@@ -47,26 +46,24 @@ class STACClient(private val endpoint: URL = new URL("https://earth-search.aws.e
                                              attributeValues: Map[String, Any], correlationId: String,
                                              processingLevel: String, page: Int): FeatureCollection = {
     val Extent(xMin, yMin, xMax, yMax) = bbox.reproject(LatLng)
-    var collectionsParam = collectionId
-    var bboxParam = Array(xMin, yMin, xMax, yMax) mkString ","
-    if (endpoint.getHost.contains("earth-search.aws.element84.com")) {
-      collectionsParam = "[\"" + collectionId + "\"]"
-      bboxParam = "[" + (Array(xMin, yMin, xMax, yMax) mkString ",") + "]"
-    }
 
-    var getProducts = http(s"$endpoint/search")
+    val (collectionsParam, bboxParam) = if (endpoint.getHost.contains("earth-search.aws.element84.com"))
+        ("[\"" + collectionId + "\"]", "[" + (Array(xMin, yMin, xMax, yMax) mkString ",") + "]")
+      else
+        (collectionId, Array(xMin, yMin, xMax, yMax) mkString ",")
+
+    val getProducts = http(s"$endpoint/search")
       .param("collections", collectionsParam)
       .param("limit", "100")
       .param("bbox", bboxParam)
       .param("page", page.toString)
 
-    if (dateRange.isDefined) {
-      getProducts = getProducts
-        .param("datetime", dateRange.get._1.format(ISO_DATE_TIME) + "/" + dateRange.get._2.format(ISO_DATE_TIME))
+    val getProductsForDateRange = dateRange.foldLeft(getProducts) { case (req, (fromDate, toDate)) =>
+      req.param("datetime", s"${fromDate format ISO_DATE_TIME}/${toDate format ISO_DATE_TIME}")
     }
 
     val json = withRetries {
-      execute(getProducts)
+      execute(getProductsForDateRange)
     }
 
     STACFeatureCollection.parse(json, toS3URL = s3URLS, dedup = true)
@@ -74,8 +71,6 @@ class STACClient(private val endpoint: URL = new URL("https://earth-search.aws.e
 
   override def getCollections(correlationId: String = ""): Seq[Feature] = {
     val getCollections = http(s"$endpoint/collections")
-      .option(HttpOptions.followRedirects(true))
-
 
     val json = withRetries {
       execute(getCollections)
