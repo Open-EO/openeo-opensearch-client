51048f3c3451d1e974caad1a25c5831adce4e57b
Jeroen Dries <jeroen.dries@vito.be>
2023-07-23 16:51:57 +0200

    left-closed intervals for oscars

--- src/main/scala/org/openeo/opensearch/backends/OscarsClient.scala
+++ src/main/scala/org/openeo/opensearch/backends/OscarsClient.scala
@@ -95,7 +95,7 @@ class OscarsClient(val endpoint: URL, val isUTM:Boolean = false) extends OpenSea
     if(dateRange.isDefined) {
       val dates = dateRange.get
       //oscars actually manages to return features that are outside of the daterange for coherence
-      val features = resultCollection.features.filter(f=>(f.nominalDate.isEqual(dates._1) || f.nominalDate.isAfter(dates._1)) && f.nominalDate.isBefore(dates._2.plusDays(1)) )
+      val features = resultCollection.features.filter(f=>f.nominalDate.isEqual(dates._1) || (f.nominalDate.isAfter(dates._1) && f.nominalDate.isBefore(dates._2))  )
       FeatureCollection(resultCollection.itemsPerPage,features)
     }else{
       resultCollection
