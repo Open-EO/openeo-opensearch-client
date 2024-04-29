1fd5b63e45fcc808cc2a4aa2a43e83a1290ca0c7
emile_sonneveld <emillo108@yahoo.com>
2024-04-29 16:04:00 +0200

    More updates since updated cache.

--- src/test/scala/org/openeo/HttpCacheTest.scala
+++ src/test/scala/org/openeo/HttpCacheTest.scala
@@ -51,7 +51,7 @@ class HttpCacheTest {
   @Test
   def testJson(): Unit = {
     HttpCache.enabled = true
-    val url = "https://catalogue.dataspace.copernicus.eu/resto/api/collections/Sentinel2/search.json?box=5.07%2C51.215%2C5.08%2C51.22&sortParam=startDate&sortOrder=ascending&page=1&maxRecords=100&status=0%7C34%7C37&dataset=ESA-DATASET&productType=L2A&processingBaseline=2.07&processingLevel=S2MSI2A&startDate=2018-03-26T00%3A00%3A00Z&completionDate=2018-04-10T00%3A00%3A00Z"
+    val url = "https://catalogue.dataspace.copernicus.eu/resto/api/collections/Sentinel2/search.json?box=5.07%2C51.215%2C5.08%2C51.22&sortParam=startDate&sortOrder=ascending&page=1&maxRecords=100&productType=L2A&startDate=2018-03-26T00%3A00%3A00Z&completionDate=2018-04-10T00%3A00%3A00Z"
 
     val sourceWarmup = Source.fromURL(new URL(url))
     sourceWarmup.getLines.mkString("\n")
@@ -63,7 +63,7 @@ class HttpCacheTest {
     val t1 = System.currentTimeMillis()
     val difference = t1 - t0
     println("time difference: " + difference)
-    assertTrue(difference < 30) // genreally it takes 2ms, but taking some margin
+    assertTrue(difference < 30) // generally it takes 2ms, but taking some margin
     assertTrue(content.startsWith("{\""))
   }
 
