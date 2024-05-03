7dd688b593d7400477836ea1ed4a1449cf88385d
emile_sonneveld <emillo108@yahoo.com>
2024-05-03 12:43:25 +0200

    test with always sorting

--- src/main/scala/org/openeo/opensearch/backends/CreodiasClient.scala
+++ src/main/scala/org/openeo/opensearch/backends/CreodiasClient.scala
@@ -133,7 +133,7 @@ class CreodiasClient(val endpoint: URL = new URL("https://catalogue.dataspace.co
       .param("dataset", "ESA-DATASET")
       .params(attributeValues.mapValues(_.toString).filterKeys(isPropagated).toSeq)
 
-    if (sorted) {
+    if (true) {
       getProducts = getProducts
         .param("sortParam", "startDate") // paging requires deterministic order
         .param("sortOrder", "ascending")
