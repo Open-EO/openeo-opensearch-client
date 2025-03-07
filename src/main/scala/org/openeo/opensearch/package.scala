ae2e8f451d43f4259696e6beaaf6581bc5f9f29e
Emile Sonneveld <emile.sonneveld@vito.be>
2025-03-07 15:47:37 +0100

    Use safeReproject to project extent to LatLon. https://github.com/Open-EO/openeo-geopyspark-driver/issues/1072

--- src/main/scala/org/openeo/opensearch/package.scala
+++ src/main/scala/org/openeo/opensearch/package.scala
@@ -1,5 +1,7 @@
 package org.openeo
 
+import geotrellis.proj4.{CRS, LatLng}
+import geotrellis.vector._
 import org.slf4j.LoggerFactory
 import scalaj.http.HttpStatusException
 import software.amazon.awssdk.core.exception.SdkClientException
@@ -57,5 +59,35 @@ package object opensearch {
     attempt(retries = 4, delay = (5, SECONDS)) {
       action
     }
+  }
+
+
+  /**
+   * Will give the same angle meaning, but as positive value.
+   */
+  def to_0_360_range(x: Double): Double = {
+    (x + 360 * 10) % 360
+  }
+
+  def safeReproject(inputProjectedExtent: ProjectedExtent, targetCrs: CRS): ProjectedExtent = {
+    if (inputProjectedExtent.crs == targetCrs) return inputProjectedExtent
+    var reprojected = inputProjectedExtent.extent.reproject(inputProjectedExtent.crs, targetCrs)
+    // TODO: Needed for webmercator too?
+    if (targetCrs == LatLng && reprojected.width > 180 && reprojected.width < 360) {
+      // Fix width wrap when projecting over anti meridian in LatLon.
+      // Reprojecting an extent could make the left and the right side swap differently over the antimeridian.
+      // A single point will always work, so consider that as the source of truth
+      // When we experience a problem because the extent does not fit in [-180, 180] range, convert it to the [0-360] range.
+      val centerReprojected = inputProjectedExtent.extent.center.reproject(inputProjectedExtent.crs, targetCrs)
+      val reprojectedCenter = reprojected.center
+      if (Math.abs(centerReprojected.x - reprojectedCenter.x) > 10) {
+        val swapped = Extent(to_0_360_range(reprojected.xmax), reprojected.ymin, to_0_360_range(reprojected.xmin), reprojected.ymax)
+        val swappedCenter = swapped.center
+        if (Math.abs(centerReprojected.x - swappedCenter.x) < 10) {
+          reprojected = swapped
+        }
+      }
+    }
+    ProjectedExtent(reprojected, targetCrs)
   }
 }
